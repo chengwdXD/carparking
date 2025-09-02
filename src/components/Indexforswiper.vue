4fa8e2d536b9197dd90bb84fce00d9c8ca0c433f
chengwdXD <4a339047@stust.edu.tw>
2025-09-02 15:54:10 +0800

    調整輪播rwd手機時高度

--- src/components/Indexforswiper.vue
+++ src/components/Indexforswiper.vue
@@ -22,7 +22,7 @@ const images = [
     class="mySwiper"
   >
     <swiper-slide v-for="(item, index) in images" :key="index">
-  <div class="relative w-full aspect-[21/9]"> 
+  <div class="relative w-full md:aspect-[21/9] h-[550px]"> 
     <img :src="item" class="absolute top-0 left-0 w-full h-full object-cover" />
   </div>
 </swiper-slide>
